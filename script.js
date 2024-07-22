bd0944d8d02f18438378f5ef664f21bf2f720153
RahulPro-1 <kumarahul1465@gmail.com>
2024-07-23 00:40:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,212 @@
+const countrylist = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+const dropdowns=document.querySelectorAll(".dropdown select");
+for(let select of dropdowns)
+    {
+        for(currCode in countrylist)
+            {
+                let newOpt=document.createElement("option");
+                newOpt.innerText=currCode;
+                newOpt.value=currCode;
+                if(select.name==="From" && currCode==="USD")newOpt.selected="selected";
+                if(select.name==="To" && currCode==="INR")newOpt.selected="selected";
+                select.append(newOpt);
+            }
+            select.addEventListener("change",(event)=>
+            {
+                updateFlag(event.target);
+            });
+    }
+    const updateFlag=(element)=>
+        {
+           let currCode=element.value;
+           let img=element.parentNode.firstElementChild;
+           img.src="https://flagsapi.com/"+countrylist[currCode] +"/flat/64.png";
+        }
+const btn=document.querySelector("form button");
+btn.addEventListener("click",(event)=>
+    {
+        event.preventDefault();
+        update();
+    });
+    async function update()
+    {
+        let amount=document.querySelector(".amount input");
+        let From=document.querySelector(".From select");
+        let To=document.querySelector(".To select");
+        if(amount.value===""||amount.value<=0)
+            {
+                amount.value="1";
+            }
+            let URL="https://v6.exchangerate-api.com/v6/d5901c6e94f4b8c7a3ba933e/latest/USD";
+            let response=await fetch(URL);
+            let data = await response.json();
+            let f=data.conversion_rates[From.value];
+            let t=data.conversion_rates[To.value];
+            let final=(t*amount.value)/f;
+            let msg=document.querySelector(".msg");
+            msg.innerText=amount.value +" " + From.value +" = " + final +" " + To.value;
+    }
+    window.addEventListener("load",()=>
+    {
+        update();
+    });
